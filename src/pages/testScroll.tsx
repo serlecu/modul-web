2b6ddc0bd31581aee257151c2c1bf2cb19408583
Sergio <serlecu@hotmail.com>
2025-05-29 19:02:27 +0200

    test preparado

---
+++ src/pages/testScroll.tsx
@@ -0,0 +1,52 @@
+import logo from '@/assets/logo.png';
+import styles from './testScroll.module.css';
+import Box from '../components/Box/Box';
+
+export default function IndexPage() {
+  const c1 = <p style={{ zIndex: '10' }}>1</p>;
+  const c2 = <p style={{ zIndex: '10' }}>2</p>;
+  const c3 = <p style={{ zIndex: '10' }}>3</p>;
+
+  return (
+    <div className={styles.app}>
+      <header>
+        <img src={logo} alt="logo" />
+        <nav>
+          <Box boxWidth={200} boxHeight={32} />
+        </nav>
+      </header>
+      <main>
+        <section id="section1">
+          <Box
+            boxWidth={200}
+            boxHeight={200}
+            strokeWidth={2}
+            notchSize={32}
+            displace={false}
+            content={c1}
+          />
+        </section>
+        <section id="section2">
+          <Box
+            boxWidth={200}
+            boxHeight={200}
+            strokeWidth={2}
+            notchSize={32}
+            content={c2}
+            layer={0}
+          />
+        </section>
+        <section id="section3">
+          <Box
+            boxWidth={200}
+            boxHeight={200}
+            strokeWidth={2}
+            notchSize={32}
+            content={c3}
+            layer={0}
+          />
+        </section>
+      </main>
+    </div>
+  );
+}
