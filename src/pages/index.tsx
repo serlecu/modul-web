30573cb32672ab6a929441c31e261cb9c439951a
Sergio <serlecu@hotmail.com>
2025-05-29 18:14:13 +0200

    fix lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import logo from '@/assets/logo.png';
 import styles from './index.module.css';
-import Box from "../components/Box/Box";
+import Box from '../components/Box/Box';
 
 export default function IndexPage() {
-
-  const c1 = <p style={{zIndex:'10'}}>HOLA</p>;
+  const c1 = <p style={{ zIndex: '10' }}>HOLA</p>;
   const c2 = <img src={logo} alt="logo" />;
 
   return (
@@ -12,33 +11,33 @@ export default function IndexPage() {
       <header>
         <img src={logo} alt="logo" />
         <nav>
-          <Box boxWidth={200} boxHeight={32}/>
+          <Box boxWidth={200} boxHeight={32} />
         </nav>
       </header>
       <main>
-        <Box 
-          boxWidth={200} 
+        <Box
+          boxWidth={200}
           boxHeight={200}
           strokeWidth={10}
           notchSize={32}
           displace={false}
           content={c1}
-          />
-        <Box 
-          boxWidth={200} 
+        />
+        <Box
+          boxWidth={200}
           boxHeight={200}
           strokeWidth={10}
           notchSize={32}
           content={c2}
           layer={0}
-          />
-        <Box 
-          boxWidth={300} 
+        />
+        <Box
+          boxWidth={300}
           boxHeight={200}
           strokeWidth={10}
           notchSize={32}
           displace={false}
-          />
+        />
       </main>
     </div>
   );
