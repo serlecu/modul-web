8c66ee0d9bf6a5de01bc55b08d6fa9b3fb1365f4
Sergio Lecuona <serlecu@hotmail.com>
2025-06-08 13:33:07 +0200

    fix useVirtualScroll

--- src/hooks/useVirtualScroll.tsx
+++ src/hooks/useVirtualScroll.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 
 /*
   Hay un conflicto con usestate y los eventos de scroll.
@@ -8,64 +8,66 @@ import React, { useState, useEffect } from 'react';
 export default function useVirtualScroll(
   lowThresh = 0,
   highThresh = 100,
-  // setter: React.Dispatch<React.SetStateAction<number>>
   ): [number, React.Dispatch<React.SetStateAction<number>>] {
-  // --- Virtual Scroll Area ---
-  let counter = 0;
-  let isDragging = false;
-  let startY = 0;
 
   const [vScrollPos, setVScrollPos] = useState(() => 0);
+  const isDragging = useRef<boolean>(false);
+  const startY = useRef<number>(0);
+
+  function updateCounter(counter: number, deltaY: number) {
+    if(counter <= lowThresh) {
+      return lowThresh+1;
+    } else if (counter >= highThresh) {
+      return highThresh-1;
+    } else {
+      return counter + deltaY
+    }
+  }
+
+  const handleWheel = useCallback((event: WheelEvent) => {
+      event.preventDefault();
+      setVScrollPos(prev => {
+        return updateCounter(prev, Math.sign(event.deltaY));
+      });
+  }, [lowThresh, highThresh]);
+
+  const handleTouchStart = useCallback((event: TouchEvent) => {
+    isDragging.current = true;
+    startY.current = event.touches[0].clientY;
+  }, []);
+  
+  const handleTouchMove = useCallback((event: TouchEvent) => {
+    event.preventDefault();
+    if (isDragging.current) {
+      const deltaY = startY.current - event.touches[0].clientY; // Vertical drag distance
+      startY.current = event.touches[0].clientY;
+      setVScrollPos(prev => {
+        return updateCounter(prev, Math.sign(deltaY));
+      });
+    }
+  }, [lowThresh, highThresh]);
+  
+  const handleTouchEnd = useCallback( () => {
+      isDragging.current = false;
+  }, []);
 
   useEffect(() => {
-    initializeVirtualScroll();
-  });
-
-
-  function initializeVirtualScroll() {
-    // --- Desktop: Wheel Event ---
-    document.addEventListener('wheel', (event) => {
-      event.preventDefault();
-      counter += Math.sign(event.deltaY);
-      if (counter < lowThresh) {
-        counter = lowThresh;
-      } else if (counter > highThresh) {
-        counter = highThresh;
-      }
-      // setVScrollPos(counter);
-      // updateCounter(counter);
-    }, { passive: false });
-
-    document.addEventListener('touchstart', (event) => {
-      isDragging = true;
-      startY = event.touches[0].clientY;
-    });
-
-    document.addEventListener('touchmove', (event) => {
-      if (isDragging) {
-        event.preventDefault();
-        const deltaY = startY - event.touches[0].clientY; // Vertical drag distance
-        counter += Math.sign(deltaY);
-        if (counter < lowThresh) {
-          counter = lowThresh;
-        } else if (counter > highThresh) {
-          counter = highThresh;
-        }
-        startY = event.touches[0].clientY;
-      }
-    });
-
-    document.addEventListener('touchend', () => {
-      isDragging = false;
-    });
-  }
-
-  function updateCounter(newCounter) {
-    // Aquí puedes actualizar el estado o hacer algo con el nuevo valor de counter
-    // setter(newCounter);
-    // setVScrollPos(newCounter);
-    console.log('Counter updated:', newCounter);
-  }
+    document.addEventListener('wheel', handleWheel, { passive: false });
+    document.addEventListener('touchstart', handleTouchStart);
+    document.addEventListener('touchmove', handleTouchMove, { passive: false });
+    document.addEventListener('touchend', handleTouchEnd);
+    return () => {
+      document.removeEventListener('wheel', handleWheel);
+      document.removeEventListener('touchstart', handleTouchStart);
+      document.removeEventListener('touchmove', handleTouchMove);
+      document.removeEventListener('touchend', handleTouchEnd);
+    };
+  }, [
+    handleWheel,
+    handleTouchStart,
+    handleTouchMove,
+    handleTouchEnd
+  ]);
 
   return [vScrollPos, setVScrollPos] as const;
 }
