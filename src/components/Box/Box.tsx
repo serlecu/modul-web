130bb09019e0ceb2e70dda568cc9685b988512a6
Sergio Lecuona <serlecu@hotmail.com>
2025-06-09 15:47:43 +0200

    improve box styling

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,55 +1,65 @@
-import styles from './box.module.css';
+import styles from './Box.module.css';
 
 interface BoxProps {
   boxWidth: number;
   boxHeight: number;
+  boxClass?: string;
   strokeWidth?: number;
   notchSize?: number;
   displace?: boolean;
-  content?: JSX.Element | undefined;
+  content?: JSX.Element;
   layer?: number;
+  posX?: number;
+  posY?: number;
 }
 
 export default function Box({
   boxWidth,
   boxHeight,
-  strokeWidth = 2,
+  boxClass,
+  strokeWidth = 1,
   notchSize = 16,
   displace = false,
-  content = undefined,
+  content,
   layer = 0,
+  posX,
+  posY,
 }: BoxProps) {
 
-  const points1 = `
-    0, ${notchSize} 
-    ${notchSize}, 0
-    ${boxWidth}, 0
-    ${boxWidth} ,${boxHeight - notchSize} 
-    ${boxWidth - notchSize},${boxHeight} 
-    0 ,${boxHeight} 
-    0, ${0 + notchSize} 
-  `;
+  const points1 = [
+    [0, notchSize],
+    [notchSize, 0],
+    [boxWidth, 0],
+    [boxWidth, boxHeight-notchSize],
+    [boxWidth-notchSize, boxHeight],
+    [0, boxHeight],
+    [0, 0 + notchSize]
+  ].map(p => p.join(',')).join(' ');
 
+  const polyClass = boxClass ? styles[boxClass] : '';
+  
   return (
-    <div className={styles.notchedBox} style={{ width: (boxWidth-notchSize), height: boxHeight-notchSize, zIndex: layer+1}}>
+    <div 
+      className={styles.notchedBox}
+      style={{
+          width: boxWidth - notchSize,
+          height: boxHeight - notchSize,
+          zIndex: layer + 1,
+          top: posY,
+          left: posX
+      }}
+    >
       <svg
         width={boxWidth}
         height={boxHeight}
-        viewBox={
-          `
-          ${0 + Math.round(notchSize * 0.5 )},
-          ${0 - Math.round(notchSize * 0.5)},
-          ${boxWidth},
-          ${boxHeight}
-          `
-        }
+        viewBox={`${Math.round(notchSize * 0.5)} ${-Math.round(notchSize * 0.5)} ${boxWidth} ${boxHeight}`}
       >
         <polygon
+          className={polyClass || styles.polyDefault}
           points={points1}
-          fill='#FFFFFFFF'
-          stroke='#000000'
           strokeWidth={strokeWidth}
         />
+      
       </svg>
       {content}
     </div>
